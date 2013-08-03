e313377826b1c94901e4569230d045767aa1d348
Yoni Plotkin <Yoniplt@gmail.com>
2013-08-03 22:04:03 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,9 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(fs.readFileSynce(Index.html).ToString());
 });
 
 var port = process.env.PORT || 5000;
