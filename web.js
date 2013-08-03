e8568e7d39f99840520af7bd08c1412a758fd7e1
Yoni Plotkin <Yoniplt@gmail.com>
2013-08-03 22:08:27 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSynce(Index.html).ToString());
+  response.send(fs.readFileSynce("index.html").ToString());
 });
 
 var port = process.env.PORT || 5000;
