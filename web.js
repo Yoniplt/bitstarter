f6f7e8d4fa7ca74e0a057b0ea80bb63943dd55e4
Yoni Plotkin <Yoniplt@gmail.com>
2013-08-04 22:49:32 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -6,10 +6,10 @@ app.use(express.logger());
 
 var buffer = fs.readFileSync('index.html');
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf-8')
+  response.send(buffer.toString('utf-8');
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
